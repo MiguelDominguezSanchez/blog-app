dd6935a25537386f324064175a2df09c269c680c
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-02-25 13:51:47 +0100

    Create Blog Posts

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { getDocs, collection } from 'firebase/firestore'
+import { db } from '../firebase-config'
 
 function Home() {
-	return <div>Home</div>
+	const [postLists, setPostlist] = useState([])
+	const postsCollectionRef = collection(db, 'posts')
+
+	useEffect(() => {
+		const getPosts = async () => {
+			const data = await getDocs(postsCollectionRef)
+			setPostlist(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+		}
+
+		getPosts()
+	})
+	return (
+		<div className='homePage'>
+			{postLists.map((post) => {
+				return (
+					<div className='post'>
+						<div className='post'>
+							<div className='postHeader'>
+								<div className='title'>
+									<h1>{post.title}</h1>
+								</div>
+							</div>
+						</div>
+						<div className='postTextContainer'>{post.postText}</div>
+						<h3>@{post.author.name}</h3>
+					</div>
+				)
+			})}
+		</div>
+	)
 }
 
 export default Home
