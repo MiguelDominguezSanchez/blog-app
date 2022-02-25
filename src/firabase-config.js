30a4abfc4dcb8cfca9cb4337c7e11523de5e559d
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-02-25 11:38:46 +0100

    Setting Routes

---
+++ src/firabase-config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firerbase/firestore'
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyCx57OsDk7rGIz0twW1AIidsMH0PKi7IxU',
+	authDomain: 'blogproject-9a400.firebaseapp.com',
+	projectId: 'blogproject-9a400',
+	storageBucket: 'blogproject-9a400.appspot.com',
+	messagingSenderId: '1088112751639',
+	appId: '1:1088112751639:web:23558c33b1306fd3221852',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+export const db = getFirestore(app)
+export const auth = getAuth(app)
+export const provider = new GoogleAuthProvider()
